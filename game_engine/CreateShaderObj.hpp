2977fa1c822f433410f965dbcc7aa4d60aeba266
chikuen <chiquanhuo@gmail.com>
2018-06-04 21:16:08 +0800

    first commit

---
+++ game_engine/CreateShaderObj.hpp
@@ -0,0 +1,15 @@
+//
+//  CreateShaderObj.hpp
+//  game_engine
+//
+//  Created by 霍志权 on 2018/6/4.
+//  Copyright © 2018年 霍志权. All rights reserved.
+//
+
+#ifndef CreateShaderObj_hpp
+#define CreateShaderObj_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+#endif /* CreateShaderObj_hpp */
---
+++ game_engine/Textfile.h
@@ -0,0 +1,22 @@
+//
+//  Textfile.h
+//  game_engine
+//
+//  Created by 霍志权 on 2018/6/4.
+//  Copyright © 2018年 霍志权. All rights reserved.
+//
+
+#ifndef Textfile_h
+#define Textfile_h
+
+
+#endif /* Textfile_h */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* textFileRead(const char *fn);
+int textFileWrite(char *fn, char *s);
+
+
